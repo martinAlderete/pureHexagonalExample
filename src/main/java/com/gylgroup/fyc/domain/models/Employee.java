fe5d992f624cc6b377bd23fd1c2b2603279c0f8f
martinAlderete <martinalderete001@gmail.com>
2025-08-26 15:32:31 -0300

    agregando models

--- src/main/java/com/gylgroup/fyc/domain/models/Employee.java
+++ src/main/java/com/gylgroup/fyc/domain/models/Employee.java
@@ -1,4 +1,43 @@
 package com.gylgroup.fyc.domain.models;
 
-public class Employee {
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class Employee extends User {
+
+    private String name;
+    private String surname;
+    private String dni;
+
+
+    public Employee(Long id, String password, String email, Set<Role> roles, boolean isEnabled, LocalDateTime creationDate, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, String name, String surname, String dni) {
+        super(id, password, email, roles, isEnabled, creationDate, isAccountNonExpired, isAccountNonLocked, isCredentialsNonExpired);
+        this.name = name;
+        this.surname = surname;
+        this.dni = dni;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
--- src/main/java/com/gylgroup/fyc/domain/models/Provider.java
+++ src/main/java/com/gylgroup/fyc/domain/models/Provider.java
@@ -1,4 +1,64 @@
 package com.gylgroup.fyc.domain.models;
 
-public class Provider {
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class Provider extends User {
+
+    private String cuit;
+    private String companyName;
+    private boolean primerIngreso;
+    private String address;
+    private Set<Employee> employees;
+    //  private City city; (FALTA IMPLEMENTAR)
+
+
+    public Provider(Long id, String password, String email, Set<Role> roles, boolean isEnabled, LocalDateTime creationDate, boolean isAccountNonExpired, boolean isAccountNonLocked, boolean isCredentialsNonExpired, String cuit, String companyName, boolean primerIngreso, String address, Set<Employee> employees) {
+        super(id, password, email, roles, isEnabled, creationDate, isAccountNonExpired, isAccountNonLocked, isCredentialsNonExpired);
+        this.cuit = cuit;
+        this.companyName = companyName;
+        this.primerIngreso = primerIngreso;
+        this.address = address;
+        this.employees = employees;
+    }
+
+    public String getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(String cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public boolean isPrimerIngreso() {
+        return primerIngreso;
+    }
+
+    public void setPrimerIngreso(boolean primerIngreso) {
+        this.primerIngreso = primerIngreso;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
 }
